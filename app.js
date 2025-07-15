5e47e54cd1448d767ee83a588866b79463c90ac2
LuisSobarzoMJ17 <luissobarzorubio1803@gmail.com>
2025-07-15 13:12:30 -0400

    Seq ID and Delete

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 const formulario = document.querySelector(".form-create");
+let contador = 0;
+let editar = false;
 
 
 formulario.addEventListener("submit", (e) => {
-    
     e.preventDefault();
 
+   
     const descripcion = document.querySelector("#descripcion").value;
     const contenedor = document.querySelector(".publicaciones");
 
     console.log(descripcion);
 
-
-    
     contenedor.innerHTML += `    
 
     <div class="publicacion">
-        <p>id: 00</p>
+        <p id="id-publicacion">id: ${contador}</p>
         <p>${descripcion}</p>
-        <button>editar</button>
-        <button>eliminar</button>
+        <button id="editar-btn" onclick="editar-btn(this)">editar</button>
+        <button id="eliminar-btn" onclick="eliminar_funt(this)">eliminar</button>
     </div>
-
     `;
+    contador++   
 
-    
 });
 
+
+function eliminar_funt(e){
+    console.log("clickeado");
+    e.parentElement.remove()
+
+    
+}
 
